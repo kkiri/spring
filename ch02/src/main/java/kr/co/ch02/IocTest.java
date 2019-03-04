958d072f18eaafe252e242f7ca4a10238c107cc1
kkiri <minju7116@naver.com>
2019-03-04 11:53:04 +0900

    2019/03/04 ch02

---
+++ ch02/src/main/java/kr/co/ch02/IocTest.java
@@ -0,0 +1,12 @@
+package kr.co.ch02;
+
+/*
+ * 날짜 : 2019/03/04
+ * 이름 : 백민주
+ * 내용 : Ioc 실습하기
+ */
+public class IocTest {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
